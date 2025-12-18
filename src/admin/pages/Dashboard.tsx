2dfb15f99973a354b9bca4c8ee36966f563a6491
RenishK25 <kalariyarenish2463@gmail.com>
2025-12-18 22:07:46 +0530

    some changes

--- src/admin/pages/Dashboard.tsx
+++ src/admin/pages/Dashboard.tsx
@@ -4,7 +4,7 @@ const Dashboard = () => {
     <div className="p-4">
       {/* Header */}
       <div className="mb-6">
-        <h1 className="text-3xl font-bold text-gray-800">Dashboard</h1>
+        <h1 className="text-3xl font-bold text-gray-800">Dashboard </h1>
       </div>
     </div>
   );
